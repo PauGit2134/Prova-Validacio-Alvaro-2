5cb9d38d5dad28e0535168f62fb8d4b8f3364f53
a24pauucllah <a24pauucllah@inspedralbes.cat>
2026-02-04 09:03:00 +0100

    Add files via upload

---
+++ electron/main.js
@@ -0,0 +1,26 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      webSecurity: false
+    }
+  });
+
+
+  win.loadFile(path.join(__dirname, 'dist', 'index.html'));
+  
+
+  win.setMenuBarVisibility(false);
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
